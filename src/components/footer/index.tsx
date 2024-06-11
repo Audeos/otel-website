f67d437970eab1adbca645745c1780a50c89a2ae
Talha Türküm ÖZKURT <eviren73@gmail.com>
2024-06-11 14:04:51 +0300

    FIX: Icons, style

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,11 +5,11 @@ import {FacebookIcon, InstagramIcon, TwitterIcon} from "../../utils/Icons.tsx";
 const Footer: React.FC = () => {
 
     return (
-        <footer className={"flex h-32 bg-cyan-100 justify-around    "}>
-            <NavLink className={"self-center w-fit h-fit"} to={"/"} end={true}>
+        <footer className={"flex h-32 bg-cyan-100"}>
+            <NavLink className={"self-center w-fit h-fit ml-10"} to={"/"} end={true}>
                 <img className={"w-40 h-auto"} src={"logo.png"} alt={"logo"}/>
             </NavLink>
-            <div className={"w-fit self-center"}>
+            <div className={"w-fit self-center ml-24"}>
                 <p className={"text-gray-700 italic"}>Tel: +90 537 309 02 20</p>
                 <p className={"text-gray-700 italic"}>E-posta: talha.turk.ozkurt@gmail.com</p>
                 <ul className={"flex self-center gap-4 mt-2"}>
@@ -20,22 +20,22 @@ const Footer: React.FC = () => {
                     </li>
                     <li>
                         <a href={"https://www.instagram.com"}>
-                            <TwitterIcon/>
+                            <InstagramIcon/>
                         </a>
                     </li>
                     <li>
                         <a href={"https://www.x.com"}>
-                            <InstagramIcon/>
+                            <TwitterIcon/>
                         </a>
                     </li>
                 </ul>
             </div>
-            <p className={"self-center text-xl"}>
+            <p className={"self-center text-xl ml-24"}>
                 <NavLink to={"/about/"}>
                     Hakkımızda
                 </NavLink>
             </p>
-            <p className={"self-center text-xl"}>
+            <p className={"self-center text-xl ml-24"}>
                 <NavLink to={"/communication/"}>
                     İletişim
                 </NavLink>
