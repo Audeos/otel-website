9666862e67c9da2a645516ce4be115f7c5d6bc70
Talha Türküm ÖZKURT <eviren73@gmail.com>
2024-06-13 08:49:20 +0300

    STYLE: HoverImage, DetailsPage, ADD: HoverImage side alignment

--- src/components/hover-image/HoverImage.tsx
+++ src/components/hover-image/HoverImage.tsx
@@ -1,29 +1,37 @@
-import React from "react";
+import React, {useCallback} from "react";
 
 type HoverImageProps = {
     src: string,
     title: string,
     description: string,
+    isAlignedRight?: boolean,
 }
 
 const HoverImage: React.FC<HoverImageProps> = (props) => {
-    const {src, description, title} = props
+    const {src, description, title, isAlignedRight = true} = props
 
+    const HoverAligned = useCallback(() => {
+        const alignClass = isAlignedRight ? "translate-x-[1000px] group-hover:translate-x-[680px]" : "translate-x-[-320px] group-hover:translate-x-[0px]"
+        return(
+            <div className={"absolute top-0 bottom-0 flex transform bg-black bg-opacity-50 transition-transform duration-500 items-center justify-center group-hover:drag-content " + alignClass}>
+                <p className="text-white font-thin text-lg p-4 w-80">
+                    {description}
+                </p>
+            </div>
+        )},
+        [description, isAlignedRight]
+    )
     return (
-        <div className="flex relative group w-[800px] overflow-hidden transform">
+        <div className="flex relative group w-[1000px] overflow-hidden transform">
             <img
                 src={src}
                 alt="History Background"
                 className="w-full object-contain"
             />
             <div className="absolute inset-0 bg-black bg-opacity-25 flex items-center justify-center">
-                <h2 className="text-white text-3xl font-bold">{title}</h2>
+                <h2 className="text-white text-3xl font-light">{title}</h2>
             </div>
-            <div className="absolute top-0 bottom-0 translate-x-[800px] transform group-hover:translate-x-[480px] bg-black bg-opacity-50 transition-transform duration-500 items-center justify-center group-hover:drag-content">
-                <p className="text-white text-xl p-4 w-80">
-                    {description}
-                </p>
-            </div>
+            <HoverAligned/>
         </div>
     )
 }
